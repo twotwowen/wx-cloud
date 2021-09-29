72f88e4394153f1dd8e83761a8179799f17ecbe3
twotwowen <two_two_wen@163.com>
2021-09-29 11:51:05 +0800

    添加首页轮播图，推荐歌曲列表

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 //自己定义的网络请求方法
 import { myRequest } from 'utils/request.js'
 wx.$myRequest = myRequest
+
 App({
     onLaunch() {
 
--- pages/index/index.js
+++ pages/index/index.js
@@ -3,19 +3,65 @@
 const app = getApp()
 Page({
     data: {
-
+        swipers: [],
+				recommended:[],
+				navIcon:[
+					{
+						icon:"iconfont icon-tuijian",
+						index:0,
+						text:'每日推荐'
+					},
+					{
+						icon:"iconfont icon-gedan",
+						index:1,
+						text:'歌单'
+					},
+					{
+						icon:"iconfont icon-paihangbang",
+						index:2,
+						text:'排行榜'
+					},
+					{
+						icon:"iconfont icon-diantai",
+						index:3,
+						text:'电台'
+					},
+					{
+						icon:"iconfont icon-zhibo",
+						index:4,
+						text:'直播'
+					}
+					
+				]
+				
     },
 
     onLoad() {
         this.getSwiper()
-
+				this.getRecommend()
     },
-    //请求网络
+    //请求轮播图
     async getSwiper() {
         const res = await wx.$myRequest({
             url: '/banner'
         })
-        console.log(res.data.banners);
-    }
+        this.setData({
+            swipers: res.data.banners
+        })
+
+        //console.log(res.data.banners);
+    },
+		
+		//请求推荐歌单
+		async getRecommend() {
+			const res = await wx.$myRequest({
+				url:'/personalized?limit=10'
+			})
+			this.setData({
+				recommended: res.data.result
+			})
+			// console.log(res)
+		}
+		
 
 })
